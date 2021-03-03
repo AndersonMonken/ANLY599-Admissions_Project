ae6162278e7ed32e9c7be6db12dc0eafc06ffa0b
Anderson Monken <anderson.e.monken@vanderbilt.edu>
2021-03-02 21:19:18 -0500

    cleaning

---
+++ data_cleaning.R
@@ -0,0 +1,17 @@
+library(tidyverse)
+
+df <- read_csv('SummerStudentAdmissions2.csv')
+
+# remove rows without a decision or that are not waitlist, admit, or decline
+df <- df %>% 
+  filter(!is.na(Decision)) %>% 
+  filter(Decision %in% c('Admit','Decline','Waitlist'))
+
+# remove rows with gpa outside of range 2-4 (standard 4.0 gpa scale)
+df <- df %>% filter(GPA <= 4 & GPA >=2)
+
+# remove rows with -1 for gender
+df <- df %>% filter(Gender != -1)
+
+# remove rows with WorkExp > 10
+df <- df %>% filter(WorkExp <= 10)
